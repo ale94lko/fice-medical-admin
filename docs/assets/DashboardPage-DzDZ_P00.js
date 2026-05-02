a780cb2ceb89969dbecc182aae69f75d110cf256
ale94lko <ale94lko@users.noreply.github.com>
2026-05-02 20:51:27 +0000

    chore: update docs

---
+++ docs/assets/DashboardPage-DzDZ_P00.js
@@ -0,0 +1,1 @@
+import{M as e,l as t}from"./runtime-core.esm-bundler-DtepgAkc.js";import{_ as n}from"./index-CPqOjbE1.js";import{t as r}from"./QPage-LD3SkaIH.js";var i={};function a(n,i){return e(),t(r,{class:`flex flex-center`})}var o=n(i,[[`render`,a]]);export{o as default};
