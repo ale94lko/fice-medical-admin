8844f59e16e492c294d805132e6154da32de6a4e
ale94lko <ale94lko@users.noreply.github.com>
2026-08-17 12:38:30 +0000

    chore: update docs

--- docs/assets/LoginLayout-B_CiHIvd.js
+++
@@ -1,1 +0,0 @@
-import{D as e,St as t,mt as n,qt as r,v as i}from"./runtime-core.esm-bundler-jaTeRxlz.js";import{a}from"./index-CW5LK_CM.js";import{i as o,t as s}from"./QLayout-CR7IRJek.js";var c={};function l(a,c){let l=t(`router-view`);return n(),i(s,{view:`hHh Lpr lff`},{default:r(()=>[e(o,null,{default:r(()=>[e(l)]),_:1})]),_:1})}var u=a(c,[[`render`,l]]);export{u as default};
---
+++ docs/assets/LoginLayout-Cul04rPb.js
@@ -0,0 +1,1 @@
+import{D as e,St as t,mt as n,qt as r,v as i}from"./runtime-core.esm-bundler-jaTeRxlz.js";import{a}from"./index-DTqY4CHo.js";import{i as o,t as s}from"./QLayout-DajYeRXC.js";var c={};function l(a,c){let l=t(`router-view`);return n(),i(s,{view:`hHh Lpr lff`},{default:r(()=>[e(o,null,{default:r(()=>[e(l)]),_:1})]),_:1})}var u=a(c,[[`render`,l]]);export{u as default};
